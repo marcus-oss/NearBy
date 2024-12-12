7badac0d857c365f15e96ebaf97d10bd47104125
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-12-12 17:23:16 -0300

    SplashScreen.kt

--- nearby/ui/Components/Screen/SplashScreen.kt
+++ nearby/ui/Components/Screen/SplashScreen.kt
@@ -7,15 +7,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.nwl.nearby.R
 import com.rocketseat.nlw.nearby.ui.theme.GreenLight
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(modifier: Modifier = Modifier) {
+fun SplashScreen(modifier: Modifier = Modifier, onNavigationToWelcome: () -> Unit) {
+
+    LaunchedEffect(key1 = Unit) {
+        delay(3_000)
+        onNavigationToWelcome()
+    }
     Box(
         modifier = Modifier
             .background(GreenLight)
@@ -37,5 +44,7 @@ fun SplashScreen(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun SplashScreenReview() {
-    SplashScreen()
+    SplashScreen(
+        onNavigationToWelcome = {}
+    )
 }
