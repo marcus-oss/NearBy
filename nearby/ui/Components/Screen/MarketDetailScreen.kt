d2bd4930baeb5feb3aa55296139b256b9d7de4cf
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-12-11 23:26:10 -0300

    Screen

---
+++ nearby/ui/Components/Screen/MarketDetailScreen.kt
@@ -0,0 +1,30 @@
+package com.nwl.nearby.ui.Screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import coil3.compose.AsyncImage
+import com.nwl.nearby.data.model.Market
+
+@Composable
+fun MarketDetailScreen(modifier: Modifier = Modifier, market: Market) {
+
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.3f),
+            contentDescription = "Imagem do Local",
+            contentScale = ContentScale.Crop,
+            model = market.cover
+        )
+    }
+
+}
+
---
+++ nearby/ui/Components/Screen/SplashScreen.kt
@@ -0,0 +1,41 @@
+package com.nwl.nearby.ui.Screen
+
+
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.nwl.nearby.R
+import com.rocketseat.nlw.nearby.ui.theme.GreenLight
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .background(GreenLight)
+            .fillMaxSize()
+    ) {
+        Image(
+            modifier = Modifier.align(Alignment.Center),
+            painter = painterResource(id = R.drawable.img_logo_logo_logo_text),
+            contentDescription = "Imagem Logo"
+        )
+        Image(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            painter = painterResource(id = R.drawable.bg_splash_screen),
+            contentDescription = "Imagem BackGround"
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenReview() {
+    SplashScreen()
+}
