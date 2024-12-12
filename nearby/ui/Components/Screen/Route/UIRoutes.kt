3d4178dce9a5f478b8188335e954e03fd20f8478
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-12-12 17:18:57 -0300

    adicionando as rotas

---
+++ nearby/ui/Components/Screen/Route/UIRoutes.kt
@@ -0,0 +1,13 @@
+package com.nwl.nearby.ui.Screen.Route
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object Splash
+
+@Serializable
+data object Welcome
+
+
+@Serializable
+data object Home
